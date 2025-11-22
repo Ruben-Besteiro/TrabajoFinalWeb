05bea65f229458ef744e8ac19c2ee9b130d7a4e0
Untar la Manteca <rbesteiro@proton.me>
2025-11-22 11:52:38 +0100

    ,

--- src/app/api/debug/route.js
+++ src/app/api/debug/route.js
@@ -1,7 +1,9 @@
 export async function GET() {
-  return Response.json({
+  return new Response(JSON.stringify({
     client: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
     redirect: process.env.SPOTIFY_REDIRECT_URI,
     scopes: process.env.SPOTIFY_SCOPES,
+  }), {
+    headers: { "Content-Type": "application/json" }
   });
 }
